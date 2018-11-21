67a8395dde6fdb6a650fc06d2101e4337bceb034
Yuta <y.nakagawa1107@gmail.com>
2018-11-21 16:22:55 +0900

    C2クリア!　同じくだがいまいち理解はできてないな 関数をいろいろ覚えねば、、、

--- word_filter.py
+++ word_filter.py
@@ -1,16 +1,18 @@
+from dataclasses import replace
+
+
 class WordFilter:
     def __init__(self, my_filter):
         self.my_filter = my_filter
 
-    def detect(self, my_filter):
+    def censor(self, my_filter):
         a = my_filter
         if "アーセナル" in a:
-            return True
-
+            return a.replace("アーセナル", "<censored>")
         else:
-            return False
-
+            return a
 
 my_filter = WordFilter("アーセナル")
-my_filter.detect("昨日のアーセナルの試合は楽しかった")
-my_filter.detect("昨日のバイエルンの試合は楽しかった")
+my_filter.censor("昨日のアーセナルの試合は楽しかった")
+print(my_filter.censor("昨日のアーセナルの試合は楽しかった"))
+print(my_filter.censor("昨日のリバプールの試合アツかった！"))
