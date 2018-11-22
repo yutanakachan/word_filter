c742dc049544267b904fa91ebbf82237f108a9b7
Yuta <y.nakagawa1107@gmail.com>
2018-11-22 12:23:31 +0900

    C5終了!　C4にトライするが出来ず･･･ また再度TRY!!!!

--- word_filter.py
+++ word_filter.py
@@ -1,18 +1,26 @@
 class WordFilter:
-    def __init__(self, my_filter):
-        self.my_filter = my_filter
+    def __init__(self, filter):
+        self.my_filter = filter
+
 
     def ng_word(self):
-        word_hit = input("NGワードにヒットしたときの表示文字を設定してください")
+        self.word = input("｢NGワードを入力してください｣"
+                          " ※何も記入がない場合はNGワード追加、終了 入力:")
+        return self.word
+
+    def ng_word_hit(self):
+        word_hit = input("｢NGワードにヒットしたときの表示文字を設定してください｣　入力:")
         return word_hit
 
     def censor(self, my_filter):
-        a = my_filter
-        if "アーセナル" in a:
-            return a.replace("アーセナル", self.ng_word())
+        word_area = my_filter
+        if "" in word_area:
+            return word_area.replace(self.ng_word(), self.ng_word_hit())
         else:
-            return a
+            return word_area
 
-my_filter = WordFilter("アーセナル")
+
+ng_list = []
+my_filter = WordFilter(filter)
 print(my_filter.censor("昨日のアーセナルの試合アツかった！"))
 print(my_filter.censor("昨日のリバプールの試合アツかった！"))
